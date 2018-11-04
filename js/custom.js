8d317e16f8263bc153d8311292148ad884ab5726
muhammad ayaz <ayazmuhammad33@gmail.com>
2018-11-04 22:24:02 +0500

    mockup web

---
+++ js/custom.js
@@ -0,0 +1,79 @@
+/* HTML document is loaded. DOM is ready.
+-------------------------------------------*/
+$(function(){
+    // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+    /* start typed element */
+    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
+    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
+    $(".element").typed({
+        strings: subElementArray,
+        typeSpeed: 30,
+        contentType: 'html',
+        showCursor: false,
+        loop: true,
+        loopCount: true,
+    });
+    /* end typed element */
+
+    /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
+    ---------------------------------------------------------------------------------*/ 
+    $('.templatemo-nav').singlePageNav({
+        offset: $(".templatemo-nav").height(),
+        filter: ':not(.external)',
+        updateHash: false
+    });
+
+    /* start navigation top js */
+    $(window).scroll(function(){
+        if($(this).scrollTop()>58){
+            $(".templatemo-nav").addClass("sticky");
+        }
+        else{
+            $(".templatemo-nav").removeClass("sticky");
+        }
+    });
+    
+    /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    /* end navigation top js */
+
+    $('body').bind('touchstart', function() {});
+
+    /* wow
+    -----------------*/
+    new WOW().init();
+});
+
+/* start preloader */
+$(window).load(function(){
+	$('.preloader').fadeOut(1000); // set duration in brackets    
+});
+/* end preloader */
